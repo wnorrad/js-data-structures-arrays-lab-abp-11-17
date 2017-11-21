72031825c2ee706d826a1408e1b36d5e5fd37480
Whitney Norrad <wnorrad@gmail.com>
2017-11-21 00:12:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Write your solution here!
+const drivers = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendDriver() {
+  drivers.push("Ralph");
+}
+
+function destructivelyPrependDriver(){
+  drivers.unshift("Bob");
+}
+
+function destructivelyRemoveLastDriver() {
+  drivers.pop();
+}
+
+function destructivelyRemoveFirstDriver() {
+  drivers.shift();
+}
+
+const drivers2 = drivers;
+
+function appendDriver() {
+return drivers2.push(name);
+}
